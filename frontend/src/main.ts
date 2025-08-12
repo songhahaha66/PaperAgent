98ee845bae17206dbc29784914d8166129a5f7b6
songhahaha66 <51509867+songhahaha66@users.noreply.github.com>
2025-08-12 23:40:04 +0800

    feat(frontend): 完成前端任务1开发，设计主页

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import * as TDesign from 'tdesign-vue-next'
 import App from './App.vue'
 import router from './router'
-import TDesign from 'tdesign-vue-next'
 import 'tdesign-vue-next/es/style/index.css'
-import pinia from './stores'
-import i18n from './lang'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 app.use(TDesign)
-app.use(pinia)
-app.use(i18n)
 
 app.mount('#app')
