086796fe035082854c75f1e74c282fb0449a20d2
songhahaha66 <songhahaha66@qq.com>
2025-08-14 00:41:07 +0800

    build(frontend): 更新 pinia-plugin-persistedstate 版本并优化存储插件集成
    - 将 pinia-plugin-persistedstate 版本从 3.2.1 升级到 4.5.0
    - 重构了 pinia 插件的初始化方式
    - 优化了 main.ts 和 stores/index.ts 中的代码结构

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,14 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import * as TDesign from 'tdesign-vue-next'
 import App from './App.vue'
 import router from './router'
+import pinia from './stores'
 import 'tdesign-vue-next/es/style/index.css'
 import 'tdesign-icons-vue-next/esm/style/index.css'
 
 const app = createApp(App)
-const pinia = createPinia()
 
-app.use(pinia as any)
+app.use(pinia)
 app.use(router)
 app.use(TDesign)
 
